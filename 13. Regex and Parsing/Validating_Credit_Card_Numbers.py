e6f0504ec018626fe36613f081391fbd6d8289c5
Avani Jindal <33371063+Avani18@users.noreply.github.com>
2020-10-30 00:53:33 +0530

    Add files via upload

---
+++ 13. Regex and Parsing/Validating_Credit_Card_Numbers.py
@@ -0,0 +1,10 @@
+import re
+test = re.compile(
+    r"^"
+    r"(?!.*(\d)(-?\1){3})"
+    r"[456]"
+    r"\d{3}"
+    r"(?:-?\d{4}){3}"
+    r"$")
+for _ in range(int(input().strip())):
+    print("Valid" if test.search(input().strip()) else "Invalid")
